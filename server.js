6d8169d27056da0734e28d49ae724b31a8fd01e0
shunpin tseng <shunpin@gmail.com>
2020-10-08 21:05:45 -0700

    initialCode

---
+++ server.js
@@ -0,0 +1,16 @@
+//initial Dependencies set up. 
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require(".routes/htmlRoutes")(app);
+
+
+//server listener 
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
