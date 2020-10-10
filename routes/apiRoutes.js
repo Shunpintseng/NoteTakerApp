4be31addf9e86d3aad3c07dd438adc0678cea7f3
shunpin tseng <shunpin@gmail.com>
2020-10-09 22:28:46 -0700

    update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,28 +1,68 @@
 //set up a const to define the data sorce saved in data folder
+var fs = require("fs");
+var notes = require("../data/note_db");
 
-const notes = require("../data/db")
+
+//create functions for adding, deleting and show notes
+function createNote (providedTitle, providedText, providedID){
+    let newNote = {title: providedTitle, text: providedText,isDeleted:false, id: providedID}
+    return newNote
+}
+
+function addNote (title, text, id){
+    let note = createNote(title, text,myDb.entries.length)
+    myDb.entries.push (note);
+    return note
+}
+
+function deleteNote (id) {
+    myDb.entries[id].isDeleted === true
+}
+
+function currentNotes () {
+    return myDb.entries.filter(function(elem){return elem.isDeleted === false});
+}
+
+function createDb (){
+    return {entries:[]}
+}
+
+var myDb = createDb()
 
 //Routing
-
 module.exports = function(app) {
 
 //GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON
 app.get("/api/notes", function(req, res) {
-    res.json(notes);
-});
+    console.log("someone requestd datat")
+    let curNotes = currentNotes();
+    console.log("printing current notes")
+    console.log(curNotes)
+    res.json(curNotes);
+  });
 
 //POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
-app.post("/api/notes", function(reg, res) {
-    notes.push(req.body);
+app.post("/api/notes", function(req, res) {
+    console.log("hello");
+    let createdNote = addNote(req.body.title, req.body.text);
+    console.log(myDb)
+    res.send()
 })
-};
+
+
+
 
 //DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 
 app.post("/api/clear", function(req, res){
-
-    
+    console.log("deleting something")
+    deleteNote(req.body.id)    
 })
+
+
+};
+
+
 
 
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,18 @@
-const path = require("path");
+var path = require("path");
+//var fs = require("fs")
 
 
 module.exports = function (app){
 
 //GET `/notes` - Should return the `notes.html` file
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__durname, "../public/notes.html"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 //GET `*` - Should return the `index.html` file
-app.get("*", function(req, res) {
+app.get("index", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 })
+
 
 }
